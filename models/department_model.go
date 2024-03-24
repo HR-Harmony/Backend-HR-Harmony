a13ee5a3387fb25059c083f8720c642b3c984490
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2024-03-25 01:17:58 +0700

    add head for department
    with employee_id

--- models/department_model.go
+++ models/department_model.go
@@ -5,6 +5,8 @@ import "time"
 type Department struct {
 	ID             uint       `gorm:"primaryKey" json:"id"`
 	DepartmentName string     `json:"department_name"`
+	EmployeeID     uint       `json:"employee_id"` // tambahan
+	FullName       string     `json:"full_name"`   // tambahan
 	CreatedAt      *time.Time `json:"created_at"`
 	UpdatedAt      time.Time
 }
