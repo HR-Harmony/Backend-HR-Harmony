194d8944fed0f87fa5c21bec89e24529657b08fd
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2024-04-26 05:59:44 +0700

    add data in performances
    + project id
    + task id
    + training id
    + goal rating
    + progress bar
    + status

--- models/goal_model.go
+++ models/goal_model.go
@@ -12,10 +12,18 @@ type Goal struct {
 	ID                uint   `gorm:"primaryKey" json:"id"`
 	GoalTypeID        uint   `json:"goal_type_id"`
 	GoalTypeName      string `json:"goal_type_name"`
+	ProjectID         uint   `json:"project_id"`
+	ProjectName       string `json:"project_name"`
+	TaskID            uint   `json:"task_id"`
+	TaskName          string `json:"task_name"`
+	TrainingID        uint   `json:"training_id"`
 	Subject           string `json:"subject"`
 	TargetAchievement string `json:"target_achievement"`
 	StartDate         string `json:"start_date"`
 	EndDate           string `json:"end_date"`
 	Description       string `json:"description"`
+	GoalRating        uint   `json:"goal_rating"`
+	ProgressBar       uint   `json:"progress_bar"`
+	Status            string `json:"status"`
 	CreatedAt         string `json:"created_at"`
 }
