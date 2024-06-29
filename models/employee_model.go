e1f3226578472d11569a373d775e3fb388c8522f
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2024-06-30 02:59:29 +0700

    bugs fix validation

--- models/employee_model.go
+++ models/employee_model.go
@@ -3,38 +3,34 @@ package models
 import "time"
 
 type Employee struct {
-	ID            uint   `gorm:"primaryKey" json:"id"`
-	PayrollID     int64  `json:"payroll_id"`
-	FirstName     string `json:"first_name"`
-	LastName      string `json:"last_name"`
-	FullName      string `json:"full_name"`
-	ContactNumber string `json:"contact_number"`
-	Gender        string `json:"gender"`
-	BirthdayDate  string `json:"birthday_date"`
-	Email         string `json:"email"`
-	Username      string `json:"username"`
-	Password      string `json:"password"`
-	ShiftID       uint   `json:"shift_id"`
-	Shift         Shift  `gorm:"foreignKey:ShiftID; constraint:OnDelete:RESTRICT" json:"shift"`
-	//Shift         string      `json:"shift"`
-	RoleID uint `json:"role_id"`
-	Role   Role `gorm:"foreignKey:RoleID; constraint:OnDelete:RESTRICT" json:"role"`
-	//Role          string      `json:"role"`
-	DepartmentID uint       `json:"department_id"`
-	Department   Department `gorm:"foreignKey:DepartmentID; constraint:OnDelete:RESTRICT" json:"department"`
-	//Department    string      `json:"department"`
+	ID            uint        `gorm:"primaryKey" json:"id"`
+	PayrollID     int64       `json:"payroll_id"`
+	FirstName     string      `json:"first_name"`
+	LastName      string      `json:"last_name"`
+	FullName      string      `json:"full_name"`
+	ContactNumber string      `json:"contact_number"`
+	Gender        string      `json:"gender"`
+	BirthdayDate  string      `json:"birthday_date"`
+	Email         string      `json:"email"`
+	Username      string      `json:"username"`
+	Password      string      `json:"password"`
+	ShiftID       uint        `json:"shift_id"`
+	Shift         string      `json:"shift"`
+	RoleID        uint        `json:"role_id"`
+	Role          string      `json:"role"`
+	DepartmentID  uint        `json:"department_id"`
+	Department    string      `json:"department"`
 	DesignationID uint        `json:"designation_id"`
-	Designation   Designation `gorm:"foreignKey:DesignationID; constraint:OnDelete:RESTRICT" json:"designation"`
-	//Designation   string      `json:"designation"`
-	BasicSalary float64     `json:"basic_salary"`
-	HourlyRate  float64     `json:"hourly_rate"`
-	PaySlipType string      `json:"pay_slip_type"`
-	IsClient    bool        `json:"is_client" gorm:"default:false"`
-	IsActive    bool        `json:"is_active" gorm:"default:true"`
-	IsExit      bool        `json:"is_exit" gorm:"default:false"`
-	PaidStatus  bool        `json:"paid_status" gorm:"default:false"`
-	Country     string      `json:"country"`
-	PayrollInfo PayrollInfo `gorm:"foreignKey:EmployeeID"`
+	Designation   string      `json:"designation"`
+	BasicSalary   float64     `json:"basic_salary"`
+	HourlyRate    float64     `json:"hourly_rate"`
+	PaySlipType   string      `json:"pay_slip_type"`
+	IsClient      bool        `json:"is_client" gorm:"default:false"`
+	IsActive      bool        `json:"is_active" gorm:"default:true"`
+	IsExit        bool        `json:"is_exit" gorm:"default:false"`
+	PaidStatus    bool        `json:"paid_status" gorm:"default:false"`
+	Country       string      `json:"country"`
+	PayrollInfo   PayrollInfo `gorm:"foreignKey:EmployeeID"`
 
 	// Details Employee - Basic Information
 	MaritalStatus string `json:"marital_status"`
