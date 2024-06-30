163cf37902ee5e49a81a8e82c98b651b2221a16c
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2024-06-30 13:51:50 +0700

    fix validation

--- models/employee_model.go
+++ models/employee_model.go
@@ -15,16 +15,16 @@ type Employee struct {
 	Username      string `json:"username"`
 	Password      string `json:"password"`
 	ShiftID       uint   `json:"shift_id"`
-	Shift         Shift  `gorm:"foreignKey:ShiftID" json:"shift"`
+	Shift         Shift  `gorm:"foreignKey:ShiftID;references:ID" json:"shift"`
 	//Shift  string `json:"shift"`
 	RoleID uint `json:"role_id"`
-	Role   Role `gorm:"foreignKey:RoleID" json:"role"`
+	Role   Role `gorm:"foreignKey:RoleID;references:ID" json:"role"`
 	//Role          string      `json:"role"`
 	DepartmentID uint       `json:"department_id"`
-	Department   Department `gorm:"foreignKey:DepartmentID" json:"department"`
+	Department   Department `gorm:"foreignKey:DepartmentID;references:ID" json:"department"`
 	//Department    string      `json:"department"`
 	DesignationID uint        `json:"designation_id"`
-	Designation   Designation `gorm:"foreignKey:DesignationID" json:"designation"`
+	Designation   Designation `gorm:"foreignKey:DesignationID;references:ID" json:"designation"`
 	//Designation   string      `json:"designation"`
 	BasicSalary float64     `json:"basic_salary"`
 	HourlyRate  float64     `json:"hourly_rate"`
